645528f86baef780f680c6721879a133d58d0268
yinghchen <yinghchen@paypal.com>
2019-07-03 21:17:31 +0800

    updated loadingPool

--- src/main/java/sample/pool/LoadingPool.java
+++ src/main/java/sample/pool/LoadingPool.java
@@ -1,44 +1,45 @@
 package sample.pool;
 
 import com.google.common.base.Preconditions;
+import io.vavr.collection.Stream;
 import io.vavr.control.Option;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 
 @Slf4j
 public class LoadingPool<T> implements Pool<T> {
-  private final int min;
-  private final int max;
+  public final static int DEFAULT_POOL_MIN_SIZE = 1;
+  public final static int DEFAULT_POOL_MAX_SIZE = 9;
+  public final static int DEFAULT_PARALLELISM = Runtime.getRuntime().availableProcessors() * 2 + 1;
+  public final static Duration DEFAULT_BORROW_TIMEOUT = Duration.ofSeconds(60L);
+  private final int minSize;
+  private final int maxSize;
+  private final Duration defaultBorrowTimeout;
   private final Supplier<T> supplier;
   private final BlockingQueue<T> queue;
   private final ExecutorService pool;
 
-  public LoadingPool(final int min, final int max, final Supplier<T> supplier) {
-    Preconditions.checkArgument(min > 0, "min is required bigger than 0");
-    Preconditions.checkArgument(max < 99, "max is required smaller than 99");
-    Preconditions.checkArgument(min < max, "max is required smaller than 99");
+  private LoadingPool(final int minSize, final int maxSize, final int parallelism, final Duration defaultBorrowTimeout, final Supplier<T> supplier) {
+    Preconditions.checkArgument(minSize > 0, "minSize is required bigger than 0");
+    Preconditions.checkArgument(maxSize < 99, "maxSize is required smaller than 99");
+    Preconditions.checkArgument(minSize < maxSize, "maxSize is required smaller than 99");
+    Preconditions.checkArgument(parallelism > 0, "parallelism is required bigger than 0");
+    Preconditions.checkNotNull(defaultBorrowTimeout, "defaultBorrowTimeout is required not null");
     Preconditions.checkNotNull(supplier, "supplier is required not null");
-    this.min = min;
-    this.max = max;
-    this.queue = new ArrayBlockingQueue<>(max);
+    this.minSize = minSize;
+    this.maxSize = maxSize;
+    this.queue = new ArrayBlockingQueue<>(maxSize);
     this.supplier = supplier;
-    this.pool = Executors.newWorkStealingPool(Runtime.getRuntime().availableProcessors() * 2 + 1);
-    init();
+    this.pool = Executors.newWorkStealingPool(parallelism);
+    this.defaultBorrowTimeout = defaultBorrowTimeout;
+    provision(minSize);
   }
 
-  private void init() {
-    final int initSize = min - queue.size();
-    if (initSize > 0) {
-      for (int i = 0; i < initSize; i++) {
-        load();
-      }
-    }
-  }
-
-  public void load() {
-    if (queue.size() < max) {
+  private void load() {
+    if (queue.size() < maxSize) {
       CompletableFuture
         .supplyAsync(supplier, pool)
         .whenComplete((t, throwable) ->
@@ -47,27 +48,75 @@ public class LoadingPool<T> implements Pool<T> {
             .toTry()
             .onFailure(x -> log.error("error while loading: {}", Option.of(throwable).map(Throwable::getMessage).getOrElse("")))
             .mapTry(queue::add)
-            .onFailure(error -> log.warn("error while adding queue: {}", error)));
+            .onFailure(exception -> log.warn("error while adding queue: {}", exception.getMessage())));
     }
   }
 
+  private void provision(final int number) {
+    Stream.from(0).takeWhile(i -> i < number).forEach(i -> load());
+  }
 
   @Override
   public T borrow(final long timeout, final TimeUnit timeUnit) {
-    load();
     try {
       return queue.poll(timeout, timeUnit);
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
+    } finally {
+      if (queue.size() < minSize) {
+        provision(minSize - queue.size());
+      }
     }
   }
 
   @Override
   public T borrow() {
-    return borrow(60L, TimeUnit.SECONDS);
+    return borrow(defaultBorrowTimeout.toMillis(), TimeUnit.MILLISECONDS);
   }
 
-  public static <T> LoadingPool<T> withSupplier(Supplier<T> supplier) {
-    return new LoadingPool<>(5, 9, supplier);
+  public static <T> Builder<T> withSupplier(final Supplier<T> supplier) {
+    return new Builder<T>(DEFAULT_POOL_MIN_SIZE, DEFAULT_POOL_MAX_SIZE, DEFAULT_PARALLELISM, DEFAULT_BORROW_TIMEOUT, supplier);
   }
+
+  public static final class Builder<T> {
+    private final int minSize;
+    private final int maxSize;
+    private final int parallelism;
+    private final Duration defaultBorrowTimeout;
+    private final Supplier<T> supplier;
+
+    private Builder(final int minSize, final int maxSize, final int parallelism, final Duration defaultBorrowTimeout, final Supplier<T> supplier) {
+      this.minSize = minSize;
+      this.maxSize = maxSize;
+      this.parallelism = parallelism;
+      this.defaultBorrowTimeout = defaultBorrowTimeout;
+      this.supplier = supplier;
+    }
+
+    public Builder<T> withMinSize(final int minSize) {
+      return new Builder<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+
+    public Builder<T> withMaxSize(final int maxSize) {
+      return new Builder<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+
+    public Builder<T> withParallelism(final int parallelism) {
+      return new Builder<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+
+    public Builder<T> withDefaultBorrowTimeout(final Duration defaultBorrowTimeout) {
+      return new Builder<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+
+    public Builder<T> withSupplier(final Supplier<T> supplier) {
+      return new Builder<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+
+    public LoadingPool<T> build() {
+      return new LoadingPool<>(minSize, maxSize, parallelism, defaultBorrowTimeout, supplier);
+    }
+  }
+
 }
+
--- src/test/java/sample/pool/PoolTest.java
+++ src/test/java/sample/pool/PoolTest.java
@@ -10,14 +10,18 @@ public class PoolTest {
 
   @Test
   public void testPool() {
-    Pool<String> pool = LoadingPool.withSupplier(() -> {
-      try {
-        TimeUnit.SECONDS.sleep(1);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      return String.valueOf(System.currentTimeMillis());
-    });
+    Pool<String> pool = LoadingPool
+      .withSupplier(() -> {
+        try {
+          TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        return String.valueOf(System.currentTimeMillis());
+      })
+      .withMinSize(5)
+      .withMaxSize(9)
+      .build();
     for (int i = 0; i < 99; i++) {
       System.out.println(String.format("%s borrowed: %s", i, pool.borrow()));
     }
