3db1bc6d4a3f5947032e870c973eca35edd219b1
yinghchen <yinghchen@paypal.com>
2019-07-31 18:48:26 +0800

    collectors.maxBy

--- src/test/java/sample/lambda/stream/StreamSample.java
+++ src/test/java/sample/lambda/stream/StreamSample.java
@@ -1,6 +1,9 @@
 package sample.lambda.stream;
 
 import com.google.common.collect.ImmutableList;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.Stream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -8,6 +11,11 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import sample.lambda.bean.Person;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamSample {
   private final Logger log = LogManager.getLogger();
@@ -18,5 +26,24 @@ public class StreamSample {
     ImmutableList
       .copyOf(new PathMatchingResourcePatternResolver().getResources("classpath*:**/*.xml"))
       .forEach(res -> log.info(res.getFilename()));
+  }
+
+  @Test
+  public void testCollectMap() {
+    final Map<Integer, List<String>> m = ImmutableList
+      .of(Tuple.of(1, "a"), Tuple.of(1, "b"), Tuple.of(2, "c"), Tuple.of(2, "d"), Tuple.of(3, "e"))
+      .stream()
+      .collect(Collectors.toMap(t -> t._1, t -> ImmutableList.of(t._2), (l, r) -> Stream.concat(l, r).collect(Collectors.toList())));
+    System.out.println(m);
+  }
+
+  @Test
+  public void testGroup() {
+    final Map<Integer, Optional<Tuple2<Integer, String>>> m = ImmutableList
+      .of(Tuple.of(1, "a"), Tuple.of(1, "b"), Tuple.of(2, "c"), Tuple.of(2, "d"), Tuple.of(3, "e"))
+      .stream()
+      .collect(Collectors.groupingBy(t -> t._1, Collectors.maxBy(Comparator.comparing(kv -> kv._2))));
+    System.out.println(m);
+
   }
 }
