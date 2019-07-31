b2d27e7f3f7ad0dadcf3398476f87e0234b093d9
yinghchen <yinghchen@paypal.com>
2019-07-31 22:38:44 +0800

    collectors.maxBy

--- src/test/java/sample/lambda/stream/StreamSample.java
+++ src/test/java/sample/lambda/stream/StreamSample.java
@@ -2,7 +2,6 @@ package sample.lambda.stream;
 
 import com.google.common.collect.ImmutableList;
 import io.vavr.Tuple;
-import io.vavr.Tuple2;
 import io.vavr.collection.Stream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -38,12 +37,22 @@ public class StreamSample {
   }
 
   @Test
-  public void testGroup() {
-    final Map<Integer, Optional<Tuple2<Integer, String>>> m = ImmutableList
+  public void testGroupBy() {
+    final Map<Integer, List<String>> m1 = ImmutableList
       .of(Tuple.of(1, "a"), Tuple.of(1, "b"), Tuple.of(2, "c"), Tuple.of(2, "d"), Tuple.of(3, "e"))
       .stream()
-      .collect(Collectors.groupingBy(t -> t._1, Collectors.maxBy(Comparator.comparing(kv -> kv._2))));
-    System.out.println(m);
+      .collect(Collectors.groupingBy(t -> t._1, Collectors.mapping(t -> t._2, Collectors.toList())));
+    System.out.println(m1);
+    final Map<Integer, Optional<String>> m2 = m1.entrySet().stream().map(kv -> Tuple.of(kv.getKey(), kv.getValue().stream().max(Comparator.naturalOrder()))).collect(Collectors.toMap(t -> t._1, t -> t._2));
+    System.out.println(m2);
+  }
 
+  @Test
+  public void testGroupByThenMax() {
+    final Map<Integer, Optional<String>> m = ImmutableList
+      .of(Tuple.of(1, "a"), Tuple.of(1, "b"), Tuple.of(2, "c"), Tuple.of(2, "d"), Tuple.of(3, "e"))
+      .stream()
+      .collect(Collectors.groupingBy(t -> t._1, Collectors.mapping(t -> t._2, Collectors.maxBy(Comparator.naturalOrder()))));
+    System.out.println(m);
   }
 }
