55fda3cbc1368a816af2aa794f1788fbe77f9f4e
ivar.chen <ivar.chen@ulala.ca>
2024-02-22 13:28:29 -0800

    fixed HttpUtil

---
+++ src/test/java/sample/basic/FsaTest.java
@@ -0,0 +1,43 @@
+package sample.basic;
+
+import com.google.common.collect.ImmutableList;
+import io.vavr.collection.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import sample.http.JsonUtil;
+
+public class FsaTest {
+
+  public static List<String> loadFsas(final String path) {
+    final List<String> fsas = ImmutableList.copyOf(ResourceUtil.readResource(path).split("\n")).stream().filter(StringUtils::isNotBlank).map(String::trim).map(String::toLowerCase).distinct().sorted().collect(Collectors.toList());
+    return fsas;
+  }
+
+  @Test
+  public void testLoad() {
+    final List<String> yegs = loadFsas("classpath:fsa/yeg.txt");
+    final List<String> yows = loadFsas("classpath:fsa/yow.txt");
+    final List<String> yuls = loadFsas("classpath:fsa/yul.txt");
+    final List<String> yvrs = loadFsas("classpath:fsa/yvr.txt");
+    final List<String> yxus = loadFsas("classpath:fsa/yxu.txt");
+    final List<String> yycs = loadFsas("classpath:fsa/yyc.txt");
+    final List<String> yyzs = loadFsas("classpath:fsa/yyz.txt");
+
+    final Map<String, List<String>> fsaCoverage = HashMap.<String, List<String>>empty()
+        .put("yeg", yegs)
+        .put("yow", yows)
+        .put("yul", yuls)
+        .put("yvr", yvrs)
+        .put("yxu", yxus)
+        .put("yyc", yycs)
+        .put("yyz", yyzs)
+        .toJavaMap();
+
+    final String dumps = JsonUtil.dump(fsaCoverage);
+    System.out.println(dumps);
+  }
+
+}
--- src/test/java/sample/basic/ShiroTest.java
+++ src/test/java/sample/basic/ShiroTest.java
@@ -1,33 +1,21 @@
 package sample.basic;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.Tuple3;
+import io.vavr.control.Try;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.apache.shiro.util.SimpleByteSource;
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.junit.Test;
-import com.google.common.base.Splitter;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
-import io.vavr.Tuple3;
-import io.vavr.collection.Vector;
-import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -36,206 +24,215 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.With;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.util.SimpleByteSource;
+import org.junit.Test;
 import sample.http.JsonUtil;
-import war.Entry;
 
 
 public class ShiroTest {
-    public static String generatePassword(String plainTextPassword, String salt) {
-        final byte[] decodedSalt = Base64.getDecoder().decode(salt);
-        String hashedPasswordBase64 = new Sha256Hash(plainTextPassword, new SimpleByteSource(decodedSalt), 1024).toBase64();
-        return hashedPasswordBase64;
-    }
 
-    @Test
-    public void testGenPasswd() {
-        final String pwd = generatePassword("admin", "RqJpLsVUWkxSr7lhyZbXEw==");
-        System.out.println(pwd);
+  public static String generatePassword(String plainTextPassword, String salt) {
+    final byte[] decodedSalt = Base64.getDecoder().decode(salt);
+    String hashedPasswordBase64 = new Sha256Hash(plainTextPassword, new SimpleByteSource(decodedSalt), 1024).toBase64();
+    return hashedPasswordBase64;
+  }
 
-        final String pwd1 = generatePassword("Password@1", "GrB8dT1biPNq3XlyweGjgw==");
-        System.out.println(pwd1);
+  @Test
+  public void testGenPasswd() {
+    final String pwd = generatePassword("admin", "vDK3TtZoyepuoqh8SWxLhQ==");
+    System.out.println(pwd);
 
-        final String pwd2 = generatePassword("Password@2", "U85lI0yQMNPY7h1iqePQOw==");
-        System.out.println(pwd2);
-    }
+    final String pwd1 = generatePassword("Password@1", "vDK3TtZoyepuoqh8SWxLhQ==");
+    System.out.println(pwd1);
 
-    @Test
-    public void testBase64() {
-        final String encoded = Base64.getEncoder().encodeToString("https://ulala-tech.atlassian.net/browse/UTT-362".getBytes());
-        System.out.println(encoded);
-        final String urlEncoded = Base64.getUrlEncoder().encodeToString("https://home-chat.vercel.app/messageList?topic=sarah_home&user=ulala".getBytes());
-        System.out.println(urlEncoded);
-        final String decoded = new String(Base64.getDecoder().decode(urlEncoded));
-        System.out.println(decoded);
-    }
+    final String pwd2 = generatePassword("Password@2", "U85lI0yQMNPY7h1iqePQOw==");
+    System.out.println(pwd2);
 
-    @Test
-    public void testUuid() {
-        final String s = UUID.randomUUID().toString();
-        System.out.println(s.substring(32));
-        System.out.println("35a9014e-d56e-44a8-b2f6-393b8bf7".length());
-        System.out.println(UUID.randomUUID().toString());
-    }
+    final String pwd3 = generatePassword("p#hxwmeQ47", "UlseZIcX6rtuQqHBnjOyzQ==");
+    System.out.println(pwd3);
+  }
 
-    private static String generateManifestName(final String station) {
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyMMddHH");
-        return String.format("%s-%s", station, LocalDateTime.now().format(dateTimeFormatter));
-    }
+  @Test
+  public void testBase64() {
+    final String encoded = Base64.getEncoder().encodeToString("https://ulala-tech.atlassian.net/browse/UTT-362".getBytes());
+    System.out.println(encoded);
+    final String urlEncoded = Base64.getUrlEncoder().encodeToString("https://home-chat.vercel.app/messageList?topic=sarah_home&user=ulala".getBytes());
+    System.out.println(urlEncoded);
+    final String decoded = new String(Base64.getDecoder().decode(urlEncoded));
+    System.out.println(decoded);
+  }
 
-    @Test
-    public void testDateFormat() {
-        final String mn = generateManifestName("YVR");
-        System.out.println(mn);
-    }
+  @Test
+  public void testUuid() {
+    final String s = UUID.randomUUID().toString();
+    System.out.println(s.substring(32));
+    System.out.println("35a9014e-d56e-44a8-b2f6-393b8bf7".length());
+    System.out.println(UUID.randomUUID().toString());
+  }
 
-    @Test
-    public void testSplitter() {
-        final Tuple2<String, String> map = Splitter.on("<>").withKeyValueSeparator(":").split("a:b").entrySet().stream().findFirst().map(Tuple::fromEntry).orElse(Tuple.of("", ""));
-        System.out.println(map);
-        Object[] objs = new Object[] { "1", "2", 3 };
-        final Tuple3<Object, Object, Object> t = Tuple.of(objs[0], objs[1], objs[2]);
-        System.out.println(t);
-    }
+  private static String generateManifestName(final String station) {
+    final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyMMddHH");
+    return String.format("%s-%s", station, LocalDateTime.now().format(dateTimeFormatter));
+  }
 
-    @Getter
-    @Setter
-    @Builder
-    @ToString
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class ManifestView {
-        private String manifest;
-        @With
-        private String status;
-        @With
-        private Long number;
-        @With
-        private Long total;
-        @With
-        private Long incomplete;
-    }
+  @Test
+  public void testDateFormat() {
+    final String mn = generateManifestName("YVR");
+    System.out.println(mn);
+  }
 
-    @Test
-    public void testMapManifestView() {
-        final ManifestView mv1 = ManifestView.builder().manifest("001").status("IN_PROGRESS").number(3L).build();
-        final ManifestView mv2 = ManifestView.builder().manifest("001").status("COMPLETED").number(6L).build();
-        final ManifestView mv3 = ManifestView.builder().manifest("001").status("IN_PROGRESS").number(5L).build();
-        final ManifestView mv4 = ManifestView.builder().manifest("002").status("IN_PROGRESS").number(5L).build();
-        final ManifestView mv5 = ManifestView.builder().manifest("002").status("MISSING").number(5L).build();
+  @Test
+  public void testSplitter() {
+    final Tuple2<String, String> map = Splitter.on("<>").withKeyValueSeparator(":").split("a:b").entrySet().stream().findFirst().map(Tuple::fromEntry).orElse(Tuple.of("", ""));
+    System.out.println(map);
+    Object[] objs = new Object[]{"1", "2", 3};
+    final Tuple3<Object, Object, Object> t = Tuple.of(objs[0], objs[1], objs[2]);
+    System.out.println(t);
+  }
 
-        final List<ManifestView> result = ImmutableList.of(mv1, mv2, mv3, mv4, mv5);
+  @Getter
+  @Setter
+  @Builder
+  @ToString
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class ManifestView {
 
-        //        final Map<String, ManifestView> viewMap = result.stream().filter(Objects::nonNull)
-        //            .map(mv -> Optional.ofNullable(mv).filter(v -> Objects.isNull(v.getStatus())).map(v -> mv.withStatus("UNKNOWN")).orElse(mv))
-        //            .collect(Collectors.toMap( // to get a map of manifest to its view
-        //                ManifestView::getManifest, // key as manifestName
-        //                mv -> ManifestView.builder() // values as ManifestView, contains manifestName, total num of Orders, and incomplete num of orders
-        //                    .manifest(mv.getManifest())
-        //                    .total(mv.getNumber())
-        //                    .incomplete(Optional
-        //                        .ofNullable(mv.getStatus())
-        //                        .filter(status -> !"COMPLETED".equalsIgnoreCase(status))
-        //                        .map(stat -> mv.getNumber())
-        //                        .orElse(0L))
-        //                    .build(),
-        //                // merge function to handle key conflicts, just keep manifest the same then sum total and incomplete
-        //                (l, r) -> ManifestView.builder().manifest(l.getManifest()).total(l.getTotal() + r.getTotal()).incomplete(l.getIncomplete() + r.getIncomplete()).build()
-        //            ));
-        //        System.out.println(viewMap);
+    private String manifest;
+    @With
+    private String status;
+    @With
+    private Long number;
+    @With
+    private Long total;
+    @With
+    private Long incomplete;
+  }
 
-        final List<ManifestView> lst = result.stream().collect(Collectors.groupingBy(ManifestView::getManifest))
-            .entrySet().stream()
-            .map(kv -> kv.getValue()
-                .stream()
-                .reduce(
-                    ManifestView.builder().manifest(kv.getKey()).total(0L).incomplete(0L).build(),
-                    (z, mv) -> z.withTotal(z.getTotal() + mv.getNumber()).withIncomplete(!"COMPLETED".equalsIgnoreCase(mv.getStatus()) ? z.getIncomplete() + mv.getNumber() : z.getIncomplete()),
-                    (z1, z2) -> z1.withTotal(z1.getTotal() + z2.getTotal()).withIncomplete(z1.getIncomplete() + z2.getIncomplete())))
-            .collect(Collectors.toList());
+  @Test
+  public void testMapManifestView() {
+    final ManifestView mv1 = ManifestView.builder().manifest("001").status("IN_PROGRESS").number(3L).build();
+    final ManifestView mv2 = ManifestView.builder().manifest("001").status("COMPLETED").number(6L).build();
+    final ManifestView mv3 = ManifestView.builder().manifest("001").status("IN_PROGRESS").number(5L).build();
+    final ManifestView mv4 = ManifestView.builder().manifest("002").status("IN_PROGRESS").number(5L).build();
+    final ManifestView mv5 = ManifestView.builder().manifest("002").status("MISSING").number(5L).build();
 
-        System.out.println(lst);
-    }
+    final List<ManifestView> result = ImmutableList.of(mv1, mv2, mv3, mv4, mv5);
 
-    @Test
-    public void testBigDecimal() {
-        final Map<String, BigDecimal> map = ImmutableMap.of("a", new BigDecimal("0.125"));
-        System.out.println(JsonUtil.dump(map));
+    //        final Map<String, ManifestView> viewMap = result.stream().filter(Objects::nonNull)
+    //            .map(mv -> Optional.ofNullable(mv).filter(v -> Objects.isNull(v.getStatus())).map(v -> mv.withStatus("UNKNOWN")).orElse(mv))
+    //            .collect(Collectors.toMap( // to get a map of manifest to its view
+    //                ManifestView::getManifest, // key as manifestName
+    //                mv -> ManifestView.builder() // values as ManifestView, contains manifestName, total num of Orders, and incomplete num of orders
+    //                    .manifest(mv.getManifest())
+    //                    .total(mv.getNumber())
+    //                    .incomplete(Optional
+    //                        .ofNullable(mv.getStatus())
+    //                        .filter(status -> !"COMPLETED".equalsIgnoreCase(status))
+    //                        .map(stat -> mv.getNumber())
+    //                        .orElse(0L))
+    //                    .build(),
+    //                // merge function to handle key conflicts, just keep manifest the same then sum total and incomplete
+    //                (l, r) -> ManifestView.builder().manifest(l.getManifest()).total(l.getTotal() + r.getTotal()).incomplete(l.getIncomplete() + r.getIncomplete()).build()
+    //            ));
+    //        System.out.println(viewMap);
 
-    }
-
-    @Test
-    public void testContains() {
-        final boolean status = ImmutableList.of(1, 2, 3).contains(null);
-        System.out.println(status);
-    }
-
-    @Builder
-    @Getter
-    @Setter
-    @ToString
-    @EqualsAndHashCode
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class DriverSettlementSnapshot {
-        @With
-        private Integer driverId;
-        @With
-        private Integer total;
-        @With
-        private Integer completed;
-        @With
-        private Integer otpEffective;
-        @With
-        private BigDecimal otp;
-    }
-
-    @Test
-    public void testBuilder() {
-        DriverSettlementSnapshot.builder().driverId(1).completed(1).otpEffective(0).otp(BigDecimal.ZERO).build();
-        //        final double sum = Vector.of(1, 2, null).toJavaList().stream()
-        //            .mapToInt(i ->i)
-        //            .sum();
-        //        System.out.println(sum);
-
-        final double d = 1.99D;
-        System.out.println(Math.floor(d));
-
-    }
-
-    public static int findMaxIndex(final List<String> names) {
-        return Optional.ofNullable(names).orElse(ImmutableList.of())
+    final List<ManifestView> lst = result.stream().collect(Collectors.groupingBy(ManifestView::getManifest))
+        .entrySet().stream()
+        .map(kv -> kv.getValue()
             .stream()
-            .filter(s -> s.contains("-"))
-            .map(s -> s.substring(s.lastIndexOf('-') + 1))
-            .mapToInt(s -> Try.of(() -> Integer.parseInt(s)).getOrElse(0))
-            .max()
-            .orElse(0);
-    }
+            .reduce(
+                ManifestView.builder().manifest(kv.getKey()).total(0L).incomplete(0L).build(),
+                (z, mv) -> z.withTotal(z.getTotal() + mv.getNumber()).withIncomplete(!"COMPLETED".equalsIgnoreCase(mv.getStatus()) ? z.getIncomplete() + mv.getNumber() : z.getIncomplete()),
+                (z1, z2) -> z1.withTotal(z1.getTotal() + z2.getTotal()).withIncomplete(z1.getIncomplete() + z2.getIncomplete())))
+        .collect(Collectors.toList());
 
-    @Test
-    public void testFindMaxIndex() {
-        final int max = findMaxIndex(ImmutableList.of("23110715-205-1", "23110715-206-2", "23110715-208-4", "23110716-111-5"));
-        System.out.println(max);
+    System.out.println(lst);
+  }
 
-        final String manifest = "YVR-23110912";
-        final String prefix = manifest.substring(0, manifest.indexOf('-'));
-        System.out.println(prefix);
-        System.out.println(ImmutableMap.of("YYZ", 1, "YVR", 2));
+  @Test
+  public void testBigDecimal() {
+    final Map<String, BigDecimal> map = ImmutableMap.of("a", new BigDecimal("0.125"));
+    System.out.println(JsonUtil.dump(map));
 
-    }
+  }
 
-    public static String extractFsa(final String postCode) {
-        return Optional.ofNullable(postCode)
-            .filter(s -> StringUtils.length(s) >= 3)
-            .map(s -> StringUtils.lowerCase(StringUtils.substring(s, 0, 3)))
-            .orElse(null);
-    }
+  @Test
+  public void testContains() {
+    final boolean status = ImmutableList.of(1, 2, 3).contains(null);
+    System.out.println(status);
+  }
 
-    @Test
-    public void testExtractFsa() {
-        final String fsa = extractFsa("V5J");
+  @Builder
+  @Getter
+  @Setter
+  @ToString
+  @EqualsAndHashCode
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class DriverSettlementSnapshot {
 
-        System.out.println(fsa);
-    }
+    @With
+    private Integer driverId;
+    @With
+    private Integer total;
+    @With
+    private Integer completed;
+    @With
+    private Integer otpEffective;
+    @With
+    private BigDecimal otp;
+  }
+
+  @Test
+  public void testBuilder() {
+    DriverSettlementSnapshot.builder().driverId(1).completed(1).otpEffective(0).otp(BigDecimal.ZERO).build();
+    //        final double sum = Vector.of(1, 2, null).toJavaList().stream()
+    //            .mapToInt(i ->i)
+    //            .sum();
+    //        System.out.println(sum);
+
+    final double d = 1.99D;
+    System.out.println(Math.floor(d));
+
+  }
+
+  public static int findMaxIndex(final List<String> names) {
+    return Optional.ofNullable(names).orElse(ImmutableList.of())
+        .stream()
+        .filter(s -> s.contains("-"))
+        .map(s -> s.substring(s.lastIndexOf('-') + 1))
+        .mapToInt(s -> Try.of(() -> Integer.parseInt(s)).getOrElse(0))
+        .max()
+        .orElse(0);
+  }
+
+  @Test
+  public void testFindMaxIndex() {
+    final int max = findMaxIndex(ImmutableList.of("23110715-205-1", "23110715-206-2", "23110715-208-4", "23110716-111-5"));
+    System.out.println(max);
+
+    final String manifest = "YVR-23110912";
+    final String prefix = manifest.substring(0, manifest.indexOf('-'));
+    System.out.println(prefix);
+    System.out.println(ImmutableMap.of("YYZ", 1, "YVR", 2));
+
+  }
+
+  public static String extractFsa(final String postCode) {
+    return Optional.ofNullable(postCode)
+        .filter(s -> StringUtils.length(s) >= 3)
+        .map(s -> StringUtils.lowerCase(StringUtils.substring(s, 0, 3)))
+        .orElse(null);
+  }
+
+  @Test
+  public void testExtractFsa() {
+    final String fsa = extractFsa("V5J");
+
+    System.out.println(fsa);
+  }
 
 }
