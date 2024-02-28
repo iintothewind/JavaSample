8206cfca0b30757c98127527a0d88ec6b2ad21be
ivar.chen <ivar.chen@ulala.ca>
2024-02-27 18:13:16 -0800

    testReplace

--- src/test/java/sample/basic/StringSample.java
+++ src/test/java/sample/basic/StringSample.java
@@ -1,13 +1,49 @@
 package sample.basic;
 
 import com.google.common.collect.ImmutableList;
+import io.vavr.collection.Vector;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class StringSample {
+
   @Test
   public void testString() {
     System.out.println(String.join("-", "2014", "1", "1"));
     System.out.println(String.join("-", ImmutableList.of("2014", "1", "1")));
+  }
+
+  @Test
+  public void testNumFormat() {
+    final Long maxDigits = 5L;
+    final String format = String.format("%%d%%0%dd_%%s", maxDigits - 1);
+    System.out.println(format);
+    System.out.println(String.format(format, 1, 1, "test"));
+  }
+
+  @Test
+  public void testReplace() {
+    final String driverIds = """
+        1085-Wen(Wendy)
+        1083-YuenYun(Vicky)
+        ftd.chi
+        1
+        Roy - Rafael
+        2006e-HARKIRAT RAJ SINGH
+        2024-YYC-A&W
+        test_a
+        jjjjjjj__asdfsadf
+        asdf%ssdf
+        bb@asdfsdf
+        asdf[ss]
+        """;
+
+    final List<String> newIds = ImmutableList.copyOf(driverIds.split("\n"))
+        .stream()
+        .map(s -> Vector.of(s.replaceAll("[\\\\(\\\\)\\.\\-_&%@\\[\\]]", " ").split("\\s+")).last())
+        .collect(Collectors.toList());
+    System.out.println(newIds);
   }
 
 }
