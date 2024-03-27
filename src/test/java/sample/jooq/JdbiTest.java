7967b7f6ba183c546219a6ddde52ba3d9f80288f
ivar <ivar.chen@ulala.ca>
2024-03-26 20:04:15 -0700

    updated test log config

--- src/test/java/sample/jooq/JdbiTest.java
+++ src/test/java/sample/jooq/JdbiTest.java
@@ -5,6 +5,7 @@ import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Slf4JSqlLogger;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -30,6 +31,7 @@ public class JdbiTest {
         Optional<Tuple2<String, String>> result = Jdbi
                 .create(DataSourceUtil.getDataSource())
                 .withHandle(handle -> handle
+                        .setSqlLogger(new Slf4JSqlLogger(log))
                         .createQuery("SELECT * FROM INFORMATION_SCHEMA.collations c where c.COLLATION_CATALOG = :catalog and c.LANGUAGE_TAG is not null")
                         .bind("catalog", "TEST")
                         .map(r -> Tuple.of(r.getColumn("COLLATION_NAME", String.class), r.getColumn("LANGUAGE_TAG", String.class)))
