ee87a9945016fd84150f524c0a908524106b9905
ivar.chen <ivar.chen@ulala.ca>
2024-05-31 17:25:01 -0700

    testTx01

--- src/test/java/sample/db/JdbiTest.java
+++ src/test/java/sample/db/JdbiTest.java
@@ -3,24 +3,24 @@ package sample.db;
 
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.statement.Slf4JSqlLogger;
 import org.junit.Test;
 
-import java.util.Optional;
-
 @Slf4j
 public class JdbiTest {
+
     @Test
     public void testQuery01() {
         Optional<Tuple2<String, String>> result = Jdbi
-                .create(DataSourceUtil.getDataSource())
-                .withHandle(handle -> handle
-                        .createQuery("SELECT * FROM INFORMATION_SCHEMA.settings s where s.SETTING_NAME = :name")
-                        .bind("name", "MODE")
-                        .map(r -> Tuple.of(r.getColumn("SETTING_NAME", String.class), r.getColumn("SETTING_VALUE", String.class)))
-                        .findOne());
+            .create(DataSourceUtil.getDataSource())
+            .withHandle(handle -> handle
+                .createQuery("SELECT * FROM INFORMATION_SCHEMA.settings s where s.SETTING_NAME = :name")
+                .bind("name", "MODE")
+                .map(r -> Tuple.of(r.getColumn("SETTING_NAME", String.class), r.getColumn("SETTING_VALUE", String.class)))
+                .findOne());
 
         System.out.println(result);
 
@@ -29,15 +29,34 @@ public class JdbiTest {
     @Test
     public void testQuery02() {
         Optional<Tuple2<String, String>> result = Jdbi
-                .create(DataSourceUtil.getDataSource())
-                .withHandle(handle -> handle
+            .create(DataSourceUtil.getDataSource())
+            .withHandle(handle -> handle
+                .setSqlLogger(new Slf4JSqlLogger(log))
+                .createQuery("SELECT * FROM INFORMATION_SCHEMA.collations c where c.COLLATION_CATALOG = :catalog and c.LANGUAGE_TAG is not null")
+                .bind("catalog", "TEST")
+                .map(r -> Tuple.of(r.getColumn("COLLATION_NAME", String.class), r.getColumn("LANGUAGE_TAG", String.class)))
+                .findFirst());
+
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void testTx01() {
+        Optional<Tuple2<String, String>> result = Jdbi
+            .create(DataSourceUtil.getDataSource())
+            .inTransaction(handle -> {
+                    //  handle.createUpdate("some sqls");
+                    final Optional<Tuple2<String, String>> collation = handle
                         .setSqlLogger(new Slf4JSqlLogger(log))
                         .createQuery("SELECT * FROM INFORMATION_SCHEMA.collations c where c.COLLATION_CATALOG = :catalog and c.LANGUAGE_TAG is not null")
                         .bind("catalog", "TEST")
                         .map(r -> Tuple.of(r.getColumn("COLLATION_NAME", String.class), r.getColumn("LANGUAGE_TAG", String.class)))
-                        .findFirst());
+                        .findFirst();
+                    return collation;
+                }
+            );
 
         System.out.println(result);
-
     }
 }
