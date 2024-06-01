bc18d34408f580bda8dfed532dbaaab789ba709f
ivar.chen <ivar.chen@ulala.ca>
2024-05-31 17:08:56 -0700

    buildTrustAllSslClient

--- src/main/java/sample/http/HttpUtil.java
+++ src/main/java/sample/http/HttpUtil.java
@@ -28,97 +28,99 @@ import okhttp3.Response;
 @Slf4j
 public class HttpUtil {
 
-  private static final X509TrustManager x509TrustManager = new X509TrustManager() {
-    @Override
-    public void checkClientTrusted(final X509Certificate[] x509Certificates, final String s) throws CertificateException {
+    private static final X509TrustManager x509TrustManager = new X509TrustManager() {
+        @Override
+        public void checkClientTrusted(final X509Certificate[] x509Certificates, final String s) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(final X509Certificate[] x509Certificates, final String s) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[]{};
+        }
+    };
+
+    private static final SSLContext trustAllSslContext;
+
+    static {
+        try {
+            trustAllSslContext = SSLContext.getInstance("SSL");
+            trustAllSslContext.init(null, new TrustManager[]{x509TrustManager}, new java.security.SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Override
-    public void checkServerTrusted(final X509Certificate[] x509Certificates, final String s) throws CertificateException {
+    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
+    public static final OkHttpClient trustAllSslClient = buildTrustAllSslClient(15L, 30L, 30L);
+
+    public static SSLContext getSslContext() {
+        return trustAllSslContext;
     }
 
-    @Override
-    public X509Certificate[] getAcceptedIssuers() {
-      return new X509Certificate[]{};
+    public static OkHttpClient buildTrustAllSslClient(final Long connectTimeout, final Long readTimeout, final Long writeTimeout) {
+        final OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(Optional.ofNullable(connectTimeout).filter(t -> t > 0).orElse(15L), TimeUnit.SECONDS)
+            .readTimeout(Optional.ofNullable(readTimeout).filter(t -> t > 0).orElse(30L), TimeUnit.SECONDS)
+            .writeTimeout(Optional.ofNullable(writeTimeout).filter(t -> t > 0).orElse(30L), TimeUnit.SECONDS)
+            .connectionPool(new ConnectionPool())
+            .sslSocketFactory(trustAllSslSocketFactory, x509TrustManager)
+            .hostnameVerifier((hostname, session) -> true)
+            .build();
+        return client;
     }
-  };
 
-  private static final SSLContext trustAllSslContext;
-
-  static {
-    try {
-      trustAllSslContext = SSLContext.getInstance("SSL");
-      trustAllSslContext.init(null, new TrustManager[]{x509TrustManager}, new java.security.SecureRandom());
-    } catch (NoSuchAlgorithmException | KeyManagementException e) {
-      throw new RuntimeException(e);
+    private static Response handleRedirect(@NonNull final OkHttpClient client, final Response tempResponse, @NonNull final Request request, @NonNull final Integer maxRedirect) {
+        if (Objects.nonNull(tempResponse) && HttpStatusClass.REDIRECTION.contains(tempResponse.code()) && maxRedirect > 0) {
+            final String redirectedUrl = tempResponse.header(HttpHeaderNames.LOCATION.toString());
+            log.info("handle redirection with url: {}", redirectedUrl);
+            if (Objects.nonNull(redirectedUrl)) {
+                final Request newReq = request.newBuilder().url(redirectedUrl).build();
+                final Response newResp = Try.of(() -> client.newCall(newReq).execute())
+                    .onFailure(t -> log.error("failed to execute redirected url: {} ", redirectedUrl, t))
+                    .getOrElse(new Response.Builder().protocol(Protocol.HTTP_1_0).request(request).code(HttpURLConnection.HTTP_INTERNAL_ERROR).message(String.format("failed to execute request with redirectUrl: %s", redirectedUrl)).build());
+                return handleRedirect(client, newResp, newReq, maxRedirect - 1);
+            }
+        }
+        return tempResponse;
     }
-  }
 
-  private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
-
-  public static final OkHttpClient trustAllSslClient = buildTrustAllSslClient(15);
-
-  public static SSLContext getSslContext() {
-    return trustAllSslContext;
-  }
-
-  public static OkHttpClient buildTrustAllSslClient(final long timeout) {
-    final OkHttpClient client = new OkHttpClient.Builder()
-        .connectTimeout(Optional.ofNullable(timeout).filter(t -> t > 0).orElse(15L), TimeUnit.SECONDS)
-        .connectionPool(new ConnectionPool())
-        .sslSocketFactory(trustAllSslSocketFactory, x509TrustManager)
-        .hostnameVerifier((hostname, session) -> true)
-        .build();
-    return client;
-  }
-
-  private static Response handleRedirect(@NonNull final OkHttpClient client, final Response tempResponse, @NonNull final Request request, @NonNull final Integer maxRedirect) {
-    if (Objects.nonNull(tempResponse) && HttpStatusClass.REDIRECTION.contains(tempResponse.code()) && maxRedirect > 0) {
-      final String redirectedUrl = tempResponse.header(HttpHeaderNames.LOCATION.toString());
-      log.info("handle redirection with url: {}", redirectedUrl);
-      if (Objects.nonNull(redirectedUrl)) {
-        final Request newReq = request.newBuilder().url(redirectedUrl).build();
-        final Response newResp = Try.of(() -> client.newCall(newReq).execute())
-            .onFailure(t -> log.error("failed to execute redirected url: {} ", redirectedUrl, t))
-            .getOrElse(new Response.Builder().protocol(Protocol.HTTP_1_0).request(request).code(HttpURLConnection.HTTP_INTERNAL_ERROR).message(String.format("failed to execute request with redirectUrl: %s", redirectedUrl)).build());
-        return handleRedirect(client, newResp, newReq, maxRedirect - 1);
-      }
+    public static <T> CheckedFunction1<? super Response, Try<T>> createHandler(@NonNull final CheckedFunction1<? super Response, T> handler) {
+        return response -> Try.success(response).filter(Objects::nonNull).mapTry(handler).andFinallyTry(response::close);
     }
-    return tempResponse;
-  }
 
-  public static <T> CheckedFunction1<? super Response, Try<T>> createHandler(@NonNull final CheckedFunction1<? super Response, T> handler) {
-    return response -> Try.success(response).filter(Objects::nonNull).mapTry(handler).andFinallyTry(response::close);
-  }
+    /**
+     * peek the response as string but not close it.
+     *
+     * @param response
+     * @return response as string
+     */
+    public static String peekResponse(final Response response) {
+        return Try.of(() -> response.peekBody(Long.MAX_VALUE).string()).getOrElse("");
+    }
 
-  /**
-   * peek the response as string but not close it.
-   *
-   * @param response
-   * @return response as string
-   */
-  public static String peekResponse(final Response response) {
-    return Try.of(() -> response.peekBody(Long.MAX_VALUE).string()).getOrElse("");
-  }
+    public static <T> T sendRequest(@NonNull final Request request, @NonNull final CheckedFunction1<? super Response, T> handler) {
+        log.info("sendRequest url: {}", request.url());
+        return Try.of(() -> trustAllSslClient.newCall(request).execute())
+            .mapTry(response -> handleRedirect(trustAllSslClient, response, request, 5))
+            .filter(Objects::nonNull)
+            .andThenTry(response -> {
+                if (response.isSuccessful()) {
+                    log.info("request succeeded with response code: {}, body: {}", response.code(), peekResponse(response));
+                } else {
+                    log.warn("request failed with response code: {}, body: {}", response.code(), peekResponse(response));
+                }
+            })
+            .flatMapTry(createHandler(handler))
+            .onFailure(t -> log.error("error while sending request: {}", request, t))
+            .getOrNull();
+    }
 
-  public static <T> T sendRequest(@NonNull final Request request, @NonNull final CheckedFunction1<? super Response, T> handler) {
-    log.info("sendRequest url: {}", request.url());
-    return Try.of(() -> trustAllSslClient.newCall(request).execute())
-        .mapTry(response -> handleRedirect(trustAllSslClient, response, request, 5))
-        .filter(Objects::nonNull)
-        .andThenTry(response -> {
-          if (response.isSuccessful()) {
-            log.info("request succeeded with response code: {}, body: {}", response.code(), peekResponse(response));
-          } else {
-            log.warn("request failed with response code: {}, body: {}", response.code(), peekResponse(response));
-          }
-        })
-        .flatMapTry(createHandler(handler))
-        .onFailure(t -> log.error("error while sending request: {}", request, t))
-        .getOrNull();
-  }
-
-  public static boolean executeRequest(@NonNull final Request request) {
-    return Optional.ofNullable(sendRequest(request, Response::isSuccessful)).orElse(false);
-  }
+    public static boolean executeRequest(@NonNull final Request request) {
+        return Optional.ofNullable(sendRequest(request, Response::isSuccessful)).orElse(false);
+    }
 }
---
+++ src/test/java/sample/mail/SimpleJavaMailTest.java
@@ -0,0 +1,7 @@
+package sample.mail;
+
+public class SimpleJavaMailTest {
+
+    
+
+}
