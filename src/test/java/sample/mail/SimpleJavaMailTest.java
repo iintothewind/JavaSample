18046f9e676d8b805baf6fe0291998e2cba41b4e
ivar.chen <ivar.chen@ulala.ca>
2024-06-03 18:00:25 -0700

    simple mail

--- src/test/java/sample/mail/SimpleJavaMailTest.java
+++ src/test/java/sample/mail/SimpleJavaMailTest.java
@@ -1,7 +1,32 @@
 package sample.mail;
 
+import org.junit.Test;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
 public class SimpleJavaMailTest {
 
-    
+
+    @Test
+    public void testSendMail() {
+        try (final Mailer mailer = MailerBuilder
+//            .withSMTPServerHost("smtp.gmail.com")
+//            .withSMTPServerPort(587)
+//            .withSMTPServerUsername("ivar.chen@ulala.ca")
+//            .withSMTPServerPassword("lkor tigb xgou mwmu")
+            .buildMailer()) {
+            mailer.sendMail(EmailBuilder.startingBlank()
+                .from("ivar.chen@ulala.ca")
+                .to("ivarchen@gmail.com")
+                .withSubject("test")
+                .withPlainText("this is a test")
+                .buildEmail());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
