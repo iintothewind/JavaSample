ce9f51f0f34847d56c286e041205732a010a986c
ivar <ivar.chen@ulala.ca>
2024-08-20 13:07:06 -0700

    JsonUtil

--- src/main/java/sample/http/JsonUtil.java
+++ src/main/java/sample/http/JsonUtil.java
@@ -21,7 +21,8 @@ public class JsonUtil {
 
     private final static ObjectMapper objectMapper = new ObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+        .setSerializationInclusion(Include.NON_NULL);
 
     public static <T> T load(final String source, @NonNull TypeReference<T> typeRef) {
         return Try.of(() -> objectMapper.readValue(source, typeRef))
@@ -30,7 +31,7 @@ public class JsonUtil {
     }
 
     public static String dump(final Object obj) {
-        return Try.of(() -> objectMapper.setSerializationInclusion(Include.NON_NULL).writeValueAsString(obj))
+        return Try.of(() -> objectMapper.writeValueAsString(obj))
             .onFailure(t -> log.error("failed to dump object: {}", obj, t))
             .getOrNull();
     }
