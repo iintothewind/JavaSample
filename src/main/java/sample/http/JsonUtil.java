86493a65fc04650159e0edbb9cadf9857eb43348
ivar <ivar.chen@ulala.ca>
2024-08-20 12:20:26 -0700

    JsonUtil

--- src/main/java/sample/http/JsonUtil.java
+++ src/main/java/sample/http/JsonUtil.java
@@ -1,5 +1,6 @@
 package sample.http;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 public class JsonUtil {
 
     private final static ObjectMapper objectMapper = new ObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
 
     public static <T> T load(final String source, @NonNull TypeReference<T> typeRef) {
         return Try.of(() -> objectMapper.readValue(source, typeRef))
@@ -28,7 +30,7 @@ public class JsonUtil {
     }
 
     public static String dump(final Object obj) {
-        return Try.of(() -> objectMapper.writeValueAsString(obj))
+        return Try.of(() -> objectMapper.setSerializationInclusion(Include.NON_NULL).writeValueAsString(obj))
             .onFailure(t -> log.error("failed to dump object: {}", obj, t))
             .getOrNull();
     }
